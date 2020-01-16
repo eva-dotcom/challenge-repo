cdc7885773bf894904313ed5c1852c6fe7eb1b41
pineappledafruitdude <59140117+pineappledafruitdude@users.noreply.github.com>
2020-01-16 08:32:59 +0100

    blablbal

--- kiapp/inc.py
+++ kiapp/inc.py
@@ -1,6 +1,6 @@
 import re, csv
 
-REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\[\]]")
+REPLACE_PUCTUATION = re.compile("[.;:!\'?,\"()\[\]]")
 REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
 
 
@@ -20,8 +20,17 @@ def preprocess_reviews(reviews):
     reviews = [re.sub(r"\'t", " not", line.lower()) for line in reviews]
     reviews = [re.sub(r"\'ve", " have", line.lower()) for line in reviews]
     reviews = [re.sub(r"\'m", " am", line.lower()) for line in reviews]
+   
 
-    reviews = [REPLACE_NO_SPACE.sub(" ", line.lower()) for line in reviews]
+    reviews = [REPLACE_PUCTUATION.sub(" ", line.lower()) for line in reviews]
     reviews = [REPLACE_WITH_SPACE.sub(" ", line) for line in reviews]
+    reviews = [re.sub('\s{2,}', ' ', line.lower()) for line in reviews]
     
     return reviews
+
+
+
+if __name__ == '__main__':
+    input = ["This isn't Chris's day he'd like to have a cow's.neck, but he doesn't like coes, didn't. \"Ente\""]
+  
+    print(preprocess_reviews(input))
